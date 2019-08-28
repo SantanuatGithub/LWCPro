fbae1fde2dda2116b0127fc459427a576d47929f
Pal <212925@cognizant.com>
2019-08-28 16:42:59 +0530

    installed lwc-jest

---
+++ .sfdx/typings/lwc/apex/LWC_ContactController.d.ts
@@ -0,0 +1,18 @@
+declare module "@salesforce/apex/LWC_ContactController.getContactList" {
+  export default function getContactList(): Promise<any>;
+}
+declare module "@salesforce/apex/LWC_ContactController.searchContactList" {
+  export default function searchContactList(param: {accountName: any}): Promise<any>;
+}
+declare module "@salesforce/apex/LWC_ContactController.getImperativeContactList" {
+  export default function getImperativeContactList(): Promise<any>;
+}
+declare module "@salesforce/apex/LWC_ContactController.searchImperativeContactList" {
+  export default function searchImperativeContactList(param: {accountName: any}): Promise<any>;
+}
+declare module "@salesforce/apex/LWC_ContactController.getStaticContact" {
+  export default function getStaticContact(): Promise<any>;
+}
+declare module "@salesforce/apex/LWC_ContactController.getImperativeStaticContact" {
+  export default function getImperativeStaticContact(): Promise<any>;
+}
